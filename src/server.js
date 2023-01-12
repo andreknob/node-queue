4b7015069fa4377f131f906bf2e2e16e0a3d4e15
André Knob <knobandre@gmail.com>
2023-01-12 14:11:03 -0300

    feat: added bull-board

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
+import { createBullBoard } from 'bull-board';
+import { ExpressAdapter, createBullBoard, BullAdapter } from '@bull-board/express';
 import UserController from './app/controllers/UserController';
+import Queue from './app/lib/Queue';
 
 const app = express();
 
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+createBullBoard({
+    queues: Queue.queues.map((queue)=> new BullAdapter(queue.bull)),
+    serverAdapter: serverAdapter,
+});
+
 app.use(express.json());
+
+app.use('/admin/queues', serverAdapter.getRouter());
 
 app.post('/users', UserController.store);
 
